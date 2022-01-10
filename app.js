c656f1bbd78e63d136eddddaa811c391fa4b988c
Your Name <you@example.com>
2022-01-10 21:03:50 +0200

    09.01.22

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 console.log("Hello world");
+//output for var: 3 3 3 
+//for(var i = 0; i < 3; i++){
+/*setTimeout(function(){
+    console.log(i);
+})
+}
+
+for(let i = 0; i < 3; i++){
+    setTimeout(function(){
+        console.log(i);
+    })
+    }*/
+
+    const fun = function(a, b) {
+return a + b
+    }
+    let a = 5;
+    console.log(a ** 2);
+    console.log(fun(10,20));
+    //sometimes there is a need to explicit conversion from string to a number
+    //"+" unary is the simpliest way for explicit conversion from strin g to a number
+    console.log(`+"12" + 12= ${+"12" + 12}`);
+   
+   /* HW # 10
+  -- using two letters. A and s, to print word "ananas" case insensititve
+  -- we have only one function wich can to do all the arifmethic actions - +,-,/,*
+  --write any example for running the folowing expression fun(5)(10,3)*/
+
