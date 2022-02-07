b105a2d1afe2fe2c0e43190302f316628c563c77
Your Name <you@example.com>
2022-02-07 21:00:49 +0200

    lection

--- app.js
+++ app.js
@@ -1,143 +1,36 @@
-class Person{
-  #id;
-  #name;
-  constructor(id, name){
-    this.#id = id;
-    this.#name = name
-  }
-  getId(){
-    return this.#id;
-  }
-  getName(){
-    return this.#name;
-  }
-  toString(){
-    return   `id: ${this.#id}, name: ${this.#name},`;      //JSON.stringify(this); - for not private settings
-  }
+const point = {
+  x: 3,
+  y: 4
+};
+function displayPoinInSpace(z, t){
+//"this" is a reference to any object having properties x andn y
+console.log(`x: ${this.x}, y: ${this.y}, z: ${z}, t: ${t}`)
 }
-const person = new Person(123, 'Vasya');
-console.log(`person is ${person}`);
-class Employee extends Person {
-  #salary;
-  constructor(id, name, salary){
-  super(id, name); // calls the constructor of class Person
-  this.#salary = salary;
-  }
-  computeSalary(){
-    return this.#salary
-  }
-  toString(){
-    return super.toString() + ` salary: ${this.computeSalary()}`;
-  }
-}
-const person2 = new Employee(124, "Sara", 16000);
-console.log(`person2 is: ${person2}`);
-console.log(typeof(person2));// just object
-console.log(person2.constructor.name)// only this way JS allows to get class name
-class Child extends Person{
-  #kinderGarden
-  constructor(id, name, kindergarden){
-    super(id, name);
-    this.#kinderGarden = kindergarden;
-  }
-  getKinderGarden(){
-    return this.#kinderGarden;
-  }
-  toString(){
-    return `${super.toString()} kindergarden: ${this.#kinderGarden}`;
-  }
-}
-const person3 = new Child(125, "Yakob", "shalom");
-console.log(`person3 is: ${person3}`);
+//displayPoinInSpace.call(point, 10, 20);
+//displayPoinInSpace.myBind(point, 10, 20)();
+//displayPoinInSpace.apply(point, [10, 20]);
 
-class WageEmployee extends Employee {
-  #hours;
-  #wage;
-  constructor(id, name, salary, hours, wage) {
-    super(id, name, salary);
-    this.#hours = hours;
-    this.#wage = wage;
-  }
-  computeSalary(){
-    return super.computeSalary() + this.#hours * this.#wage
-  }
-}
-const person4 = new WageEmployee(126, 'Asaf', 7000, 10, 1000);
-console.log(`person4 is; ${person4}`);
+//arguments are passed at function call
 
- 
- 
- console.log('********HOMEWORK**********');
-  //*************************1******************** */ 
- function countOfType(persons, type){
-   const res = persons.reduce((sum, n )=> {n.constructor.name === type ? sum += 1: sum; return sum;}, 0);
-   return res;
-   //return person.filter(p => p.constructor.name === name) <-----definition of Yuriy
- //returns count of persons of the given type}
- //Example: countOfPersonType(persons(its must be string), "WageEmployee") ----> 1
- }
- //**************************2********************** */
- function computeSalaryBudget(persons){
-  //return persons.reduce((sum, per) => {per.constructor.name !== "Child" ? sum += per.computeSalary() : sum;
-  //return sum}, 0)
-  const allEmloyees = persons.filter(p => !!p.computeSalary);
-const salaryValues = allEmloyees.map(p => p.computeSalary());
-return salaryValues.reduce((res, cur)=> res = cur)
-  //const allEmloyees = persons.filter(p => p.computeSalary)
-  //return allEmloyees.reduce((res, cur) => res + cur.computeSalary(),0) <-----------Yuriy
-  // return total salary of all employees in the given array}
- // Example: computeSalaryBudget(persons) ----> 3000}
+const funDisplay = displayPoinInSpace.myBind(point);
+funDisplay(10, 20);
+//all arguments are bound by the method myBind
+const funDisplayArguments = displayPoinInSpace.myBind(point, 10, 20);
+funDisplayArguments();
+
+//mixed - part of arguments are bound by the method myBind and other part of args passed at function call
+const funDisplayMixed = displayPoinInSpace.myBind(point, 10);
+funDisplayMixed(20);
+
+ Function.prototype.myBind = function(thisObj,...args){
+   //"this" - reference to any functional object(in example - displayPoint)
+   //thisObj - reference to any object(in example - point)
+   return (...params) => {
+    thisObj.method123 = this;
+    const res = thisObj.method123(...args.concat(params))
+    delete thisObj.method123;
+    return res;
    }
-   //*********************3************************ */
-   function countChildrenInGarden(persons, garden){
-   return persons.reduce((res, per) => {per.constructor.name === "Child" 
-   && per.getKinderGarden() === garden
-    && res++; 
-    return res}, 0)
-  //const allChildren = getPersonsType(persons, 'Child');
-  //return allChildren.reduce((res, cur) => cur.getKinderGarden() === kindergarden? res + 1 : res, 0) ---- > Yuriy
- //returns number of children in the given array}
- // Example: countChildrenInGarden(persons, Shalom) ----> 2
  }
-
-
-//     TEST FUNCTIONALITY
-const persons = [
-  new Child(100, 'Pol', "Shalom"), new Child(101, "Sergey", "Boker"),
-  new Child(102, "Gabi", "Shalom"), new Employee(103, "Niv", 10000), 
-  new WageEmployee(104, "Nik", 10000, 10, 90)];
-    
-
- console.log(`Choosed type: ${countOfType(persons, "Child")}`);
- console.log(`Salary Budget is: ${computeSalaryBudget(persons)}`);
- console.log(`Children in garden are: ${countChildrenInGarden(persons, "Shalom")}`);
-
-
- function Deferred() {
-  this.functions = []
-}
-Deferred.prototype.then = function(thenFn) {
-  this.functions.push(thenFn);
-}
-Deferred.prototype.resolve = function(res) {
-  this.functions.forEach(fn => res = fn(res));
-
-}
-   const d = new Deferred()
-d.then(function(res){ console.log('1 ', res); return 'a'; });
-
-d.then(function(res){ console.log('2 ', res); return 'b'; });
-
-d.then(function(res){ console.log('3 ', res); return 'c'; });
-d.resolve('hello');
-
-
-
-
-function testOutput(funct, expected){
-  console.log(`logging of parameter this`)
-console.log(`function ${funct.name}; expected result: ${expected} ; actual result : ${funct()}`)
-}
-//testOutput(WageEmployee.prototype.computeSalary.bind(person4), 2000)  //<----------------------------BIND
-testOutput(computeSalaryBudget.bind(undefined,persons))
-testOutput(countOfType.bind(undefined,persons, 'Child'))
+ point.method = displayPoinInSpace;
+ point.method(10,20)
