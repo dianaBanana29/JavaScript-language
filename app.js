ec4fc7b62d6d691b5bca36b2da563a4b1df42cb5
Your Name <you@example.com>
2022-02-13 20:53:39 +0200

    checked with Daniel

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-function getRandomNumber(min, max) {
-  let float = min + Math.random() * (max + 1 - min);// for what to swap, if its works without?
-  return Math.round(float);
-}
-
-function getRandomNumber1(min, max) {
-  return min > max ? Math.floor(Math.random() * (min - max + 1)) + max : Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 function getRandomNumber2(min, max) {
   if(min > max){[min, max] = [max, min]}
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function concatinate(prefix) {
+function concatenate(prefix) {
  return status =>  prefix + status;
  }
 
@@ -20,7 +11,7 @@ function concatinate(prefix) {
 function test() {
 console.log(getRandomNumber2(15, 1));
 console.log(getRandomNumber1(15, 1)); 
-const concatApp = concatinate('App - ')
+const concatApp = concatenate('App - ')
 const concatMessage = concatApp('Test status: Done');
 console.log(concatMessage);
 }
